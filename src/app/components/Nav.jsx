2b95fae81c9541b99e0154f036d3d6e766152609
FoLkky60 <111549090+FoLkky60@users.noreply.github.com>
2024-05-19 04:17:38 +0700

    Navbar

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { FaCannabis } from "react-icons/fa";
+import React from 'react';
+import './Nav.css';
 
 function Nav() {
-
   return (
-    <div className="flex justify-center">
-        <nav className='w-full flex justify-between items-center h-24 bg-slate-800' >
-            <a href="/"><FaCannabis className='ml-4 size-16 text-white' /></a>
-            <ul className='flex mr-20'>
-                <li><a className='mr-10 text-white text-xl font-sans' href="#">How to use</a></li>
-                <li><a className='text-white text-xl font-sans' href="#">Teams</a></li>
-            </ul>
-        </nav>
+    <div>
+      <div className='w-full h-96 bg-custom bg-slate-800 bg-opacity-60 rounded-bl-[35px] rounded-br-[35px] p-10 flex flex-col justify-between'>
+        <div className='flex justify-between items-center'>
+          <div className='text-2xl text-white'>AIMAI</div>
+          <div className='flex text-white text-xl font-sans'>
+            <div className='mr-10'>Home</div>
+            <div>Content</div>
+          </div>
+          <div>
+            <a className='flex text-white text-xl font-sans border border-white rounded-3xl px-4 py-2' href="#">
+              Sign in
+            </a>
+          </div>
+        </div>
+        <div className='flex-grow flex flex-col justify-center items-center'>
+          <div className='text-white text-3xl font-sans mb-4 m-10'>
+            VERIFICATION WITH AI DETECTION
+          </div>
+          <div className='m-6 text-white text-l font-sans border border-white rounded-3xl px-4 py-2'>
+            ADEIO
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
